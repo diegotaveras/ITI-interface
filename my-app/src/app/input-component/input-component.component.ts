dbfd6d8043141b027f079c9c60c34f6e944c935e
diegotaveras <diegoat2@illinois.edu>
2023-05-25 16:57:02 -0400

    created app; made input form component, and stylized components

---
+++ my-app/src/app/input-component/input-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-input-component',
+  templateUrl: './input-component.component.html',
+  styleUrls: ['./input-component.component.css']
+})
+export class InputComponentComponent {
+  @Output() filesSelected = new EventEmitter<FileList>();
+
+  handleFileChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement.files) {
+      this.filesSelected.emit(inputElement.files);
+    }
+  }
+}
