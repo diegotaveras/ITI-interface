ef2497d34fe0598e1322fff8fd7d0b1eac982e44
diegotaveras <diegoat2@illinois.edu>
2023-06-01 17:04:42 -0400

    xmlparser.py; progress on python interface dev mode

---
+++ Python/xmlparser.py
@@ -0,0 +1,14 @@
+import xmltodict
+import json
+xml_file=open("policy.xml","r")
+xml_string=xml_file.read()
+python_dict=xmltodict.parse(xml_string)
+json_string=json.dumps(python_dict, indent=4)
+
+out = open("output.json", "w")
+out.write(json_string)
+out.close()
+
+print("The given .xml file given in .json format: output.xml")
+
+
